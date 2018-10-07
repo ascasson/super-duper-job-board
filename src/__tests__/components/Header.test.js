b1a36f8dca4cc4de33f7f06a1c65f3da5eacac2d
Ben Halverson <benhalverson33@gmail.com>
2018-10-07 16:13:43 -0700

    feat(tests header dashboard): setup testing, created a header, created a dashboard component
    #4 #7

---
+++ src/__tests__/components/Header.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme'
+import toJSON from 'enzyme-to-json';
+import Header from '../../components/Header';
+
+test('should render header correctly', () => {
+  const wrapper = shallow(<Header />);
+  expect(toJSON(wrapper)).toMatchSnapshot();
+});
