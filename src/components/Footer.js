67a6ca55ff9fd4032ff63598406fc2453c4cc886
Ricky Prahatama <ricky.prahatama@gmail.com>
2018-10-08 21:43:02 +0700

    Added App footer

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './footer.css';
+import { NavLink } from 'react-router-dom';
+
+const Footer = () => (
+  <footer className="App-footer">
+    <div>
+      <img src="http://via.placeholder.com/150x50" alt="Super Duper App Logo" />
+      <nav>
+        <h3>Links</h3>
+        <ul>
+          <li>
+            <NavLink to="/" activeClassName="is-active" exact={true}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/create" activeClassName="is-active">
+              Add a job
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </footer>
+);
+
+export default Footer;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import Dashboard from '../components/Dashboard';
 
 const AppRouter = () => (
@@ -10,6 +11,7 @@ const AppRouter = () => (
     <Switch>
     <Route path="/" component={Dashboard} exact={true}/>
     </Switch>
+    <Footer />
   </div>
   
   </BrowserRouter>
